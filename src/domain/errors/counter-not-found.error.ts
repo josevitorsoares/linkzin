e406b2cf3ea9b425f0c09a8aa36b1dc2ed8467f1
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2025-05-09 18:38:21 -0300

    ♻️ refactor: add application custom error structure

--- src/domain/errors/counter-not-found.error.ts
+++ src/domain/errors/counter-not-found.error.ts
@@ -1,7 +1,11 @@
-export class CounterNotFoundError extends Error {
+import { AppError } from "./app-error.error";
+
+export class CounterNotFoundError extends AppError {
   constructor() {
     super();
     this.name = "CounterNotFoundError";
     this.message = "Counter not found";
+    this.statusCode = 404;
+    this.stack = new Error().stack;
   }
 }
--- src/domain/errors/url-not-found.error.ts
+++ src/domain/errors/url-not-found.error.ts
@@ -1,7 +1,11 @@
-export class UrlNotFoundError extends Error {
+import { AppError } from "./app-error.error";
+
+export class UrlNotFoundError extends AppError {
   constructor() {
     super();
     this.name = "UrlNotFoundError";
     this.message = "Url not found";
+    this.statusCode = 404;
+    this.stack = new Error().stack;
   }
 }
