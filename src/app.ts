f4bf77aa1b624f2f486b7fc9ff2e058e708064a2
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2025-05-09 12:08:50 -0300

    ✨ feat: add error fastify handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import { connectDatabase } from "@configs/database/mongoose.database";
+import { CounterNotFoundError } from "@errors/counter-not-found.error";
+import { UrlNotFoundError } from "@errors/url-not-found.error";
 import fastify from "fastify";
 import { urlRoutes } from "./infrastructure";
 
@@ -8,3 +10,18 @@ app.register(urlRoutes);
 
 // # Connect to MongoDB
 connectDatabase();
+
+// This is a global error handler for the Fastify app.
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof UrlNotFoundError || CounterNotFoundError) {
+    reply.status(404).send({
+      message: error.message,
+    });
+  }
+
+  reply.status(500).send({
+    message: "Internal server error",
+    error: error.message,
+  });
+});
+ 
