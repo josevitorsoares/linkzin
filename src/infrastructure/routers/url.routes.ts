21de4f030bdb04cc24472793bd9e22125acf299b
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2025-05-09 12:08:25 -0300

    ✨ feat: add get shortened url route

--- src/infrastructure/routers/url.routes.ts
+++ src/infrastructure/routers/url.routes.ts
@@ -1,7 +1,16 @@
-import { makeShortUrlController } from "@factories/controllers";
+import {
+  makeGetShortenedUrlController,
+  makeShortUrlController,
+} from "@factories/controllers";
 import { FastifyInstance } from "fastify";
 
 export const urlRoutes = async (app: FastifyInstance) => {
+  app.get("/:hash", async (request, reply) => {
+    const shortUrlController = makeGetShortenedUrlController();
+
+    return shortUrlController.handle(request, reply);
+  });
+
   app.post("/create", async (request, reply) => {
     const shortUrlController = makeShortUrlController();
 
